1068dfd7f9c0a0ebabe8f1c6cfefc1092645b25a
Marcos Guillen <10079595+marcos-guillen@users.noreply.github.com>
2024-12-30 15:46:04 +0100

    Puestas las instrucciones y ejemplos.

--- reto10.js
+++ reto10.js
@@ -45,7 +45,23 @@ function compile(instructions) {
       return undefined
     return Number(register[0]);
   }
+/*
+Los elfos programadores están creando un pequeño ensamblador mágico para controlar las máquinas del taller de Santa Claus.
 
+Para ayudarles, vamos a implementar un intérprete sencillo que soporte las siguientes instrucciones mágicas:
+
+    MOV x y: Copia el valor x (puede ser un número o el contenido de un registro) en el registro y
+    INC x: Incrementa en 1 el contenido del registro x
+    DEC x: Decrementa en 1 el contenido del registro x
+    JMP x y: Si el valor del registro x es 0 entonces salta a la instrucción en el índice y y sigue ejecutándose el programa desde ahí.
+
+Comportamiento esperado:
+
+    Si se intenta acceder, incrementar o decrementar a un registro que no ha sido inicializado, se tomará el valor 0 por defecto.
+    El salto con JMP es absoluto y lleva al índice exacto indicado por y.
+    Al finalizar, el programa debe devolver el contenido del registro A. Si A no tenía un valor definido, retorna undefined.
+
+*/
   const instructions = [
     "MOV -1 C", // copia -1 al registro 'C',
     "INC C", // incrementa el valor del registro 'C'
@@ -55,3 +71,4 @@ function compile(instructions) {
   ];
   
   compile(instructions); // -> 2
+  // Nota: Los registros que no han sido inicializados previamente se inicializan a 0.
